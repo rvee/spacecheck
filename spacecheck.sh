c6b04d69493632b6255dfdd75280d663696d4a36
Arvind Kapuganti <rvee.ash@gmail.com>
2017-10-31 02:57:38 +0530

    added spacecheck script

---
+++ spacecheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function testpath {
+  echo "Checking $1"
+  val=`sudo du -xh --max-depth=1 $1 | sort -h | tail -n 4`
+  echo "$val"
+  nextpath=`echo "$val" | tail -n2 | head -n1 | cut -f2`
+}
+
+if [ -z $1 ]
+then
+    testpath "/"
+else
+    testpath $1
+fi
